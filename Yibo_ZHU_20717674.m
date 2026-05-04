919abef1317d1c66564227b41dd937bb1ffe98b9
biyyz182 <biyyz182@nottingham.edu.cn>
2026-05-05 01:39:48 +0800

    Test

--- Yibo_ZHU_20717674.m
+++ Yibo_ZHU_20717674.m
@@ -1,2 +1,14 @@
 % Linking MATLAB to GitHub
 % CW2
+% Yibo ZHU
+% biyyz182@nottingham.edu.cn
+a = arduino('COM3','Uno');
+writeDigitalPin(a,'D2',1);
+pause(1);
+writeDigitalPin(a,'D2',0);
+for k = 1:10
+    writeDigitalPin(a,'D2',1);
+    pause(0.5);
+    writeDigitalPin(a,'D2',0);
+    pause(0.5);
+end
