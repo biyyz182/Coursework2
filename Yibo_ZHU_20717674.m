df01f7bd1c38e891ac92b332c52c546ed5d0e4be
biyyz182 <biyyz182@nottingham.edu.cn>
2026-05-05 10:56:36 +0800

    updated

--- Yibo_ZHU_20717674.m
+++ Yibo_ZHU_20717674.m
@@ -2,6 +2,7 @@
 % CW2
 % Yibo ZHU
 % biyyz182@nottingham.edu.cn
+% Test
 a = arduino('COM3','Uno');
 writeDigitalPin(a,'D2',1);
 pause(1);
@@ -12,3 +13,15 @@ for k = 1:10
     writeDigitalPin(a,'D2',0);
     pause(0.5);
 end
+% Task 1
+duration=600;                
+n_samples=duration;          
+voltage_data=zeros(n_samples,1);  
+temp_data=zeros(n_samples,1);
+fprintf('Starting temperature acquisition for %d seconds...\n', duration);
+for i = 1:n_samples
+    voltage_data(i) = readVoltage(a, 'A0');        
+    temp_data(i) = (voltage_data(i) - 0.5) / 0.01; 
+    pause(1);                                       
+end
+fprintf('Acquisition complete.\n');
